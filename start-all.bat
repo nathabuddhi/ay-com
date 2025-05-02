e6a6c08c1c75554c062e7c0ba19fc483873d8d0c
Natha Buddhi Pratama Chandra <nathabuddhi@gmail.com>
2025-05-02 16:36:32 +0700

    start-all.bat fix

--- start-all.bat
+++ start-all.bat
@@ -7,5 +7,5 @@ new-tab -d .\backend\service-notification --title "NOTIFICATION" cmd /k "go buil
 new-tab -d .\backend\service-thread --title "THREAD" cmd /k "go build -o main.exe && main.exe" ; ^
 new-tab -d .\backend\service-media --title "MEDI" cmd /k "go build -o main.exe && main.exe" ; ^
 new-tab -d .\backend\util-email --title "EMAIL" cmd /k "go build -o main.exe && main.exe" ; ^
-new-tab -d .\backend\util-redis --title "REDIS" cmd /k "go build -o main.exe && main.exe" 
-new-tab -d .\backend\api-gateway --title "GATEWAY" cmd /k "go build -o main.exe && main.exe" ; ^
+new-tab -d .\backend\util-redis --title "REDIS" cmd /k "go build -o main.exe && main.exe" ; ^
+new-tab -d .\backend\api-gateway --title "GATEWAY" cmd /k "go build -o main.exe && main.exe" 
