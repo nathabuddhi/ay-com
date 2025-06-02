b3956fc55f393bd88d416256913c3071a24f545b
Natha Buddhi Pratama Chandra <nathabuddhi@gmail.com>
2025-06-02 16:14:47 +0700

    Update start-all.bat script so that the frontend only initializes after every service does.

--- start-all.bat
+++ start-all.bat
@@ -1,7 +1,6 @@
 @echo off
 wt ^
 new-tab --title "WSL" cmd /k "wsl" ; ^
-new-tab -d .\frontend --title "Frontend" cmd /k "npm run dev" ; ^
 new-tab -d .\backend\service-user --title "USER" cmd /k "go build -o main.exe && main.exe" ; ^
 new-tab -d .\backend\service-notification --title "NOTIFICATION" cmd /k "go build -o main.exe && main.exe" ; ^
 new-tab -d .\backend\service-thread --title "THREAD" cmd /k "go build -o main.exe && main.exe" ; ^
@@ -9,3 +8,4 @@ new-tab -d .\backend\service-media --title "MEDI" cmd /k "go build -o main.exe &
 new-tab -d .\backend\util-email --title "EMAIL" cmd /k "go build -o main.exe && main.exe" ; ^
 new-tab -d .\backend\util-redis --title "REDIS" cmd /k "go build -o main.exe && main.exe" ; ^
 new-tab -d .\backend\api-gateway --title "GATEWAY" cmd /k "go build -o main.exe && main.exe" 
+new-tab -d .\frontend --title "Frontend" cmd /k "npm run dev" ; ^
